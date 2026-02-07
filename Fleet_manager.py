3912b7382cb4519f80680c98a3a316f7a63bc6b0
CyrusGD592 <cg592@kent.ac.uk>
2026-02-07 13:59:50 +0000

    menu start

--- Fleet_manager.py
+++ Fleet_manager.py
@@ -3,6 +3,20 @@ r = ["Captain", "Commander", "Lt. Commander", "Lieutenant", "Lieutenant"]
 d = ["Command", "Command", "Operations", "Security", "Engineering"]
 f = ["10", "12", "14", "18", "22"]
 
+def display_menu(): 
+        name = input("User Full Name: ") 
+        print("MENU: ") 
+        print("User: " + name) 
+        print("1: Display Roster -") 
+
+        opt = input("Select Option: ") 
+        if opt == "1": 
+                display_roster()
+
+def display_roster(): 
+        print("Current Crew Roster -")
+        for i in range(len(n)): 
+                print() 
 
 
 def init_database()  :
@@ -10,7 +24,8 @@ def init_database()  :
                 print(n[i] + ": " + r[i] + " - " + d[i] + " #" + f[i])
 
 
-def main():
+def main():  
+        display_menu()
         init_database()   
 
 
