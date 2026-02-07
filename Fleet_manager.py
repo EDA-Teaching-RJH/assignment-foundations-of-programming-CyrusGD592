610265eaf68b8fd0a496624557578bb67b9381b2
CyrusGD592 <cg592@kent.ac.uk>
2026-02-07 14:37:28 +0000

    menu option 2 start

--- Fleet_manager.py
+++ Fleet_manager.py
@@ -2,31 +2,44 @@ n = ["Picard", "Riker", "Data", "Worf", "La Forge"]
 r = ["Captain", "Commander", "Lt. Commander", "Lieutenant", "Lieutenant"]
 d = ["Command", "Command", "Operations", "Security", "Engineering"]
 f = ["10", "12", "14", "18", "22"]
-
+name = input("User Full Name: ")
 def display_menu(): 
-        name = input("User Full Name: ") 
+        
         print("MENU: ") 
         print("User: " + name) 
-        print("1: Display Roster -") 
+        print("1: Display Roster -")  
+        print("2: Add Crew -")
 
         opt = input("Select Option: ") 
         if opt == "1": 
-                display_roster()
+                display_roster()  
+
+        if opt == "2":
+
 
 def display_roster(): 
-        print("Current Crew Roster -")
+        print("Current Crew Roster -") 
+        print("Name:  Rank:   Divison:   Id:")
         for i in range(len(n)): 
-                print() 
+                print(n[i] + ": " + r[i] + " - " + d[i] + " #" + f[i])  
+        display_menu() 
 
 
-def init_database()  :
+def init_database()  : 
+ print("Welcome")  
+ print("Initialising Database . . . .")
+ print("Current Crew: ")
  for i in range(len(n)): 
-                print(n[i] + ": " + r[i] + " - " + d[i] + " #" + f[i])
+                print(n[i] + ": " + r[i] + " - " + d[i] + " #" + f[i]) 
+ print("Loading Menu . . . . .") 
+
+                
+
 
 
 def main():  
-        display_menu()
         init_database()   
+        display_menu() 
 
 
 main() 
